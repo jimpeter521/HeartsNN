4c7833946c2a912b6604da2ca7d9e94f85e19ec6
Jim Lloyd <jim.lloyd@gmail.com>
2020-03-21 13:52:21 -0700

    More flexible build configuration

--- cmake/buildconfigs/release.cmake
+++ cmake/buildconfigs/release.cmake
@@ -1,4 +1,3 @@
-set(CMAKE_CXX_STANDARD 11)
 set(CMAKE_BUILD_TYPE Release CACHE STRING "" FORCE)
 
 find_program(CMAKE_C_COMPILER gcc)
@@ -11,21 +10,3 @@ endif()
 if(NOT CMAKE_CXX_COMPILER)
   message(FATAL_ERROR "Cannot find C++ compiler")
 endif()
-
-set(
-    CMAKE_C_COMPILER
-    "${CMAKE_C_COMPILER}"
-    CACHE
-    STRING
-    "C compiler"
-    FORCE
-)
-
-set(
-    CMAKE_CXX_COMPILER
-    "${CMAKE_CXX_COMPILER}"
-    CACHE
-    STRING
-    "C++ compiler"
-    FORCE
-)
